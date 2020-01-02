3df93e6acdc19abb028377b88606a9a480808c74
Gleb Mirolyubov <g.mirolyubov@se15.qmul.ac.uk>
2020-01-03 00:39:09 +0600

    Added turret control script

---
+++ Spacetor/Assets/Scripts/TurretRotation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    private Touch touch;
+    private float rotationSpeed = 0.2f;
+    private Quaternion rotationZ;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+            float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+            transform.Rotate(Vector3.forward, XaxisRotation);
+        }
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            float touchRatioY = touch.position.y / Screen.height;
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (touchRatioY < 0.5f)
+                {
+                    rotationZ = Quaternion.Euler(0f, 0f, touch.deltaPosition.x * rotationSpeed);
+                } else
+                {
+                    rotationZ = Quaternion.Euler(0f, 0f, -touch.deltaPosition.x * rotationSpeed);
+                }
+
+                transform.rotation = rotationZ * transform.rotation;
+            }
+
+        }
+    }
+
+    }
